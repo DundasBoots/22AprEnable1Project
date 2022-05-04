0d450d2a2dacfd2c30949844d0ef99bba64e602a
DundasBoots <junjielee@live.co.uk>
2022-05-04 10:06:26 +0100

    updated domain item class to inlcude value of item to satisfy user story of being able to calculate total cost

--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -2,18 +2,23 @@ package com.qa.ims.persistence.domain;
 
 public class Item {
 	private Long id;
+	private Double value;
 	private String itemName;
 
-	public Item(String itemName) {
+	
+	public Item(Double value, String itemName) {
 		super();
+		this.value = value;
 		this.itemName = itemName;
 	}
 
-	public Item(Long id, String itemName) {
+	public Item(Long id, Double value, String itemName) {
 		super();
 		this.id = id;
+		this.value = value;
 		this.itemName = itemName;
 	}
+
 
 	public Long getId() {
 		return id;
@@ -21,6 +26,14 @@ public class Item {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
 	}
 
 	public String getItemName() {
@@ -33,7 +46,7 @@ public class Item {
 
 	@Override
 	public String toString() {
-		return "id:" + id + " item name:" + itemName;
+		return "id:" + id + " item name:" + itemName + "value:" + value;
 
 	}
 
@@ -42,6 +55,7 @@ public class Item {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((itemName == null) ? 0 : itemName.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
@@ -59,6 +73,11 @@ public class Item {
 			if (other.getItemName() != null)
 				return false;
 		} else if (!getItemName().equals(other.getItemName()))
+			return false;
+		if (getValue() == null) {
+			if (other.getValue() != null)
+				return false;
+		} else if (!getValue().equals(other.getValue()))
 			return false;
 		if (id == null) {
 			if (other.id != null)
