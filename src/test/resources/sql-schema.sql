36fb49d435d509d1b0c4176ed230ed8eb21929b0
Jordan <103116636+DundasBoots@users.noreply.github.com>
2022-05-06 16:01:44 +0100

    Updated SQL schema for order tests

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,8 +1,18 @@
-DROP TABLE IF EXISTS `customers`;
+ DROP TABLE IF EXISTS `customers`;
 
-CREATE TABLE IF NOT EXISTS `customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) DEFAULT NULL,
+ CREATE TABLE IF NOT EXISTS `customers` (
+     `id` INT(11) NOT NULL AUTO_INCREMENT,
+     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-);
+     PRIMARY KEY (`id`)
+ );
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `order_id` INT(11) NOT NULL,
+    `customer` VARCHAR(40) DEFAULT NULL,
+    `item` VARCHAR(40) DEFAULT NULL,
+    PRIMARY KEY (`id`)	
+); 
+
+    
