7175284ee7057b413ac3a82dac0e19be02b3de40
Jordan <103116636+DundasBoots@users.noreply.github.com>
2022-05-06 16:02:43 +0100

    Update SQL data and schema for item tests

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,1 +1,3 @@
 INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `items` (`item_name`, `value`) VALUES ('Car', 200.00);
+INSERT INTO `orders` (`order_id`,`customer`, `item`) VALUES (1,'Jordan', 'Car');
